c1a0aba0a9f0d4f6e07bfcf3bd7dee64419ff3cc
Mout Pessemier <moutpessemier@hotmail.com>
2019-11-25 15:47:29 +0100

    Initial Commit

---
+++ GameHub-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameHub-iOS
+//
+//  Created by Mout Pessemier on 25/11/2019.
+//  Copyright © 2019 Digitized. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GameHub-iOSTests/GameHub_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  GameHub_iOSTests.swift
+//  GameHub-iOSTests
+//
+//  Created by Mout Pessemier on 25/11/2019.
+//  Copyright © 2019 Digitized. All rights reserved.
+//
+
+import XCTest
+@testable import GameHub_iOS
+
+class GameHub_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
