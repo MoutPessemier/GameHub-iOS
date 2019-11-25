093c0a870b255e8690303f8c9fb6e9462c4fde66
Mout Pessemier <moutpessemier@hotmail.com>
2019-11-25 15:54:34 +0100

    V1.0.0 - Cocoapods [FEAT] - added Cocoapods to my app

---
+++ Pods/Target Support Files/Pods-GameHub-iOS-GameHub-iOSUITests/Pods-GameHub-iOS-GameHub-iOSUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GameHub_iOS_GameHub_iOSUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GameHub_iOS_GameHub_iOSUITests
+@end
---
+++ Pods/Target Support Files/Pods-GameHub-iOSTests/Pods-GameHub-iOSTests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_GameHub_iOSTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GameHub_iOSTestsVersionString[];
+
